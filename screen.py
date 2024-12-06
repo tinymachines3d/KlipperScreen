72a677b4a4b9e9c817fe3ab5d8c1a3f82fe02836
TM3D <chris@tinymachines3d.com>
2024-12-06 07:31:40 -0600

    change script path for V2.3.4 of CB1

--- screen.py
+++ screen.py
@@ -504,6 +504,7 @@ class KlipperScreen(Gtk.Window):
         )
 
     def _button_pressed_feedback(self, widget=None):
+        os.system('/boot/scripts/ks_click.sh')
         os.system('/etc/scripts/ks_click.sh')
     
     def load_custom_theme(self, theme_name):
